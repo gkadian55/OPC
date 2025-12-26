b83b731e170845bb7f18226ad500af9cb852e57d
Gaurav <gkadian55@gmail.com>
2025-12-26 21:25:18 +0530

    Solved question 5 of the problem set

--- OPC - 1/E_Santa_Magic_Workshop_Progress_Bar.java
+++ OPC - 1/E_Santa_Magic_Workshop_Progress_Bar.java
@@ -4,8 +4,18 @@ public class E_Santa_Magic_Workshop_Progress_Bar {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int[] tiles = new int[n];
         int k = sc.nextInt();
         int t = sc.nextInt();
+        t = (t * n * k) / 100;
+        int i = 0;
+        while (t > 0) {
+            if (t >= k) tiles[i] = k;
+            else tiles[i] = t;
+            t -= k;
+            i++;
+        }
+        for (int a : tiles) System.out.print(a + " ");
         sc.close();
     }
 }
